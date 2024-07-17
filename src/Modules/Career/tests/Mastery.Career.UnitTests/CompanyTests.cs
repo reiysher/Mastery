d48af1b597db1f0bcb6cb868f5fb1c8cf3dba019
reiysher <reiysher25@gmail.com>
2024-07-17 23:26:06 +0300

    test(company): create company test

---
+++ src/Modules/Career/tests/Mastery.Career.UnitTests/CompanyTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Mastery.Career.UnitTests;
+
+public sealed class CompanyTests
+{
+    [Fact]
+    public void The_company_is_correctly_created()
+    {
+        // Arrange
+        var sut = new Company();
+
+        // Act
+
+
+        // Assert
+    }
+}
