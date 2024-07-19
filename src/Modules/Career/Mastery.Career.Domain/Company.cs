3a468d467b4e0e9c3600cbf34650d94fdc7c5656
reiysher <reiysher25@gmail.com>
2024-07-19 23:10:17 +0300

    feat(company): add change rank method and test

--- src/Modules/Career/Mastery.Career.Domain/Company.cs
+++ src/Modules/Career/Mastery.Career.Domain/Company.cs
@@ -1,5 +1,25 @@
-﻿namespace Mastery.Career.Domain;
+﻿
+namespace Mastery.Career.Domain;
 
 public sealed class Company
 {
+    private Company() { }
+
+    public Guid Id { get; init; }
+
+    public Guid? RankId { get; private set; }
+
+    public static Company New(Guid id)
+    {
+        return new Company()
+        {
+            Id = id,
+            RankId = null,
+        };
+    }
+
+    public void SetRank(Guid rankId)
+    {
+        RankId = rankId;
+    }
 }
--- src/Modules/Career/tests/Mastery.Career.UnitTests/CompanyTests.cs
+++ src/Modules/Career/tests/Mastery.Career.UnitTests/CompanyTests.cs
@@ -8,11 +8,27 @@ public sealed class CompanyTests
     public void The_company_is_correctly_created()
     {
         // Arrange
-        var sut = new Company();
+        Guid companyId = Guid.NewGuid();
 
         // Act
-
+        Company sut = Company.New(companyId);
 
         // Assert
+        sut.Id.Should().Be(companyId);
+        sut.RankId.Should().BeNull();
+    }
+
+    [Fact]
+    public void Company_rank_was_changed()
+    {
+        // Arrange
+        Guid rankId = Guid.NewGuid();
+        Company sut = Company.New(Guid.NewGuid());
+
+        // Act
+        sut.SetRank(rankId);
+
+        // Assert
+        sut.RankId.Should().Be(rankId);
     }
 }
