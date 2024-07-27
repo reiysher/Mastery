9d88dae4ea71306c51f825135826b87812ecd0e6
reiysher <reiysher25@gmail.com>
2024-07-28 00:22:34 +0300

    feat(users): add user repository

---
+++ src/Mastery.Career.Domain/Users/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Mastery.Career.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(UserId userId, CancellationToken cancellationToken = default);
+
+    void Add(User user);
+}
