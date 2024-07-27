202105f7cde36c8b4347ae5f036cc649683e9539
reiysher <reiysher25@gmail.com>
2024-07-27 16:46:15 +0300

    refactor(architecture): use clean architecture

--- src/Modules/Career/Mastery.Career.Domain/Color.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mastery.Career.Domain;
-
-public sealed record Color
-{
-    public static Color Default = new Color();
-
-    private Color() { }
-}
--- src/Modules/Career/Mastery.Career.Domain/CompanyExceptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Mastery.Career.Domain;
-
-internal static class CompanyExceptions
-{
-    public const string InvalidIdInput = "Input id has a default value";
-}
