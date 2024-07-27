e17661805ecb9cb01f6f88a1868f99ee2bdb8d61
reiysher <reiysher25@gmail.com>
2024-07-27 22:07:09 +0300

    feat(companies): add company id

---
+++ src/Mastery.Career.Domain/Companies/CompanyId.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Mastery.Career.Domain.Companies;
+
+public sealed record CompanyId
+{
+    public required Guid Value { get; init; }
+
+    [SetsRequiredMembers]
+    private CompanyId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static CompanyId New()
+    {
+        return new CompanyId(Guid.NewGuid());
+    }
+
+    public static CompanyId From(Guid? value)
+    {
+        if (!value.HasValue)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException(nameof(value));
+        }
+
+        return new CompanyId(value.Value);
+    }
+
+    public static CompanyId Copy(CompanyId? other)
+    {
+        if (other == null)
+        {
+            throw new ArgumentNullException(nameof(other));
+        }
+
+        return new CompanyId(other.Value);
+    }
+}
