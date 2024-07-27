fe4997fc3be462dff5ffed8edb5f69e05912275a
reiysher <reiysher25@gmail.com>
2024-07-27 22:07:20 +0300

    feat(jobs): add job id

---
+++ src/Mastery.Career.Domain/Jobs/JobId.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Mastery.Career.Domain.Jobs;
+
+public sealed record JobId
+{
+    public required Guid Value { get; init; }
+
+    [SetsRequiredMembers]
+    private JobId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static JobId New()
+    {
+        return new JobId(Guid.NewGuid());
+    }
+
+    public static JobId From(Guid? value)
+    {
+        if (!value.HasValue)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException(nameof(value));
+        }
+
+        return new JobId(value.Value);
+    }
+
+    public static JobId Copy(JobId? other)
+    {
+        if (other == null)
+        {
+            throw new ArgumentNullException(nameof(other));
+        }
+
+        return new JobId(other.Value);
+    }
+}
