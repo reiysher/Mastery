75e1ad4b46d1224b10a25b138384f0172ec542d5
reiysher <reiysher25@gmail.com>
2024-07-28 00:47:55 +0300

    chore(results): add result and error classes

---
+++ src/Mastery.Career.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Mastery.Career.Domain.Abstractions;
+
+public sealed record Error(string Code, string Name)
+{
+    public static Error None = new(string.Empty, string.Empty);
+
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
+}
