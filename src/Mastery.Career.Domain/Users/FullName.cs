ca4b558788aaa212b2973031c95a11671bc5316e
reiysher <reiysher25@gmail.com>
2024-07-28 00:11:02 +0300

    feat(users): add user aggregate

---
+++ src/Mastery.Career.Domain/Users/FullName.cs
@@ -0,0 +1,22 @@
+﻿namespace Mastery.Career.Domain.Users;
+
+public sealed record FullName
+{
+    private FullName() { }
+
+    public required string FirstName { get; init; }
+
+    public required string LastName { get; init; }
+
+    public static FullName From(string firstName, string lastName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(firstName, nameof(firstName));
+        ArgumentException.ThrowIfNullOrWhiteSpace(lastName, nameof(lastName));
+
+        return new FullName
+        {
+            FirstName = firstName,
+            LastName = lastName,
+        };
+    }
+}
---
+++ src/Mastery.Career.Domain/Users/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Mastery.Career.Domain.Users;
+
+public sealed class User : Entity<UserId>
+{
+    private User() { }
+
+    public FullName Name { get; private set; } = default!;
+
+    public Email Email { get; private set;} = default!;
+
+    public static User Create(string firstName, string lastName, string email)
+    {
+        return new User
+        {
+            Id = UserId.New(),
+            Name = FullName.From(firstName, lastName),
+            Email = Email.Create(email),
+        };
+    }
+}
