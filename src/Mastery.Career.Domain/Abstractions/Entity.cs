a3c3e6e24d1a57743e5fb0f5110395c76bbdd6b2
reiysher <reiysher25@gmail.com>
2024-07-28 00:07:02 +0300

    chore(entity): add equality implementations

--- src/Mastery.Career.Domain/Abstractions/Entity.cs
+++ src/Mastery.Career.Domain/Abstractions/Entity.cs
@@ -1,7 +1,57 @@
 ﻿namespace Mastery.Career.Domain.Abstractions;
 
-public abstract class Entity<T>
-    where T : notnull
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
 {
-    public required T Id { get; init; }
+    public required TId Id { get; init; }
+
+    public static bool operator ==(Entity<TId>? left, Entity<TId>? right)
+    {
+        return left is not null && right is not null && left.Equals(right);
+    }
+
+    public static bool operator !=(Entity<TId>? left, Entity<TId>? right)
+    {
+        return !(left == right);
+    }
+
+    public bool Equals(Entity<TId>? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (other is not Entity<TId> entity)
+        {
+            return false;
+        }
+
+        return other.Id.Equals(Id);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (obj is not Entity<TId> entity)
+        {
+            return false;
+        }
+
+        return entity.Id.Equals(Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
