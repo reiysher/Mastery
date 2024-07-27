643fa56befd3137f5191b7d63f5ae60315d1be8d
reiysher <reiysher25@gmail.com>
2024-07-27 23:43:04 +0300

    feat(category): add category aggreagte

---
+++ src/Mastery.Career.Domain/Categories/Category.cs
@@ -0,0 +1,23 @@
+﻿namespace Mastery.Career.Domain.Categories;
+
+public sealed class Category : Entity<CategoryId>
+{
+    public Category() { }
+
+    public string Value { get; private set; } = default!;
+
+    public Color Color { get; private set; } = default!;
+
+    public static Category Create(Guid id, string value, Color color)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentNullException.ThrowIfNull(color);
+
+        return new Category
+        {
+            Id = CategoryId.From(id),
+            Value = value,
+            Color = color,
+        };
+    }
+}
---
+++ src/Mastery.Career.Domain/Categories/Color.cs
@@ -0,0 +1,16 @@
+﻿namespace Mastery.Career.Domain.Categories;
+
+public sealed record Color
+{
+    private Color() { }
+
+    public required string Value { get; init; }
+
+    public static Color New(string value)
+    {
+        return new Color
+        {
+            Value = value
+        };
+    }
+}
