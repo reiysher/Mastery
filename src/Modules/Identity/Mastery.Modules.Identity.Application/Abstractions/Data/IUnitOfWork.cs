1811e2193f57dc0c08d677a3fe3f2991911df89c
reiysher <reiysher25@gmail.com>
2024-08-04 22:31:51 +0300

    feat(users): add register user use case

---
+++ src/Modules/Identity/Mastery.Modules.Identity.Application/Abstractions/Data/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Mastery.Modules.Identity.Application.Abstractions.Data;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Modules/Identity/Mastery.Modules.Identity.Infrastructure/IdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Mastery.Modules.Identity.Application.Abstractions.Data;
+using Mastery.Modules.Identity.Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mastery.Modules.Identity.Infrastructure;
+
+internal sealed class IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+    : DbContext(options), IUnitOfWork
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("identity");
+        
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+    }
+}
