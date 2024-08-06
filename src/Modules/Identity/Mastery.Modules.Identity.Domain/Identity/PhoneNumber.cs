ab158f0418fafe29e7613eae6bf43a2c7093219b
reiysher <reiysher25@gmail.com>
2024-08-07 00:17:08 +0300

    feat(identity): add phone number

---
+++ src/Modules/Identity/Mastery.Modules.Identity.Domain/Identity/PhoneNumber.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using Mastery.Common.Domain;
+
+namespace Mastery.Modules.Identity.Domain.Identity;
+
+public sealed partial record PhoneNumber
+{
+    private const string Pattern = "^[0-9]+$";
+
+    public string CountryCode { get; private set; }
+
+    public string Value { get; private set; }
+
+    public bool Confirmed { get; private set; }
+
+    private PhoneNumber() { }
+
+    internal static Result<PhoneNumber> From(string? countryCode, string? phoneNumber)
+    {
+        if (string.IsNullOrWhiteSpace(countryCode) || !countryCode.StartsWith('+'))
+        {
+            return Result.Failure<PhoneNumber>(UserErrors.InvalidPhoneNumber);
+        }
+
+        if (string.IsNullOrWhiteSpace(phoneNumber) || phoneNumber.Length != 10 || !PhoneNumberValidationRegex().IsMatch(phoneNumber))
+        {
+            return Result.Failure<PhoneNumber>(UserErrors.InvalidPhoneNumber);
+        }
+
+        return new PhoneNumber
+        {
+            CountryCode = countryCode,
+            Value = phoneNumber,
+            Confirmed = false,
+        };
+    }
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex PhoneNumberValidationRegex();
+}
